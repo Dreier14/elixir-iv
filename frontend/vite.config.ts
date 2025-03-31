7e371f5e2e186ea947a83af38bb82417001e60e1
Adam Dreier <adamdreier@adams-mbp.mynetworksettings.com>
2025-03-31 09:11:37 -0700

    Add SSR

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,20 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    ssr: 'src/entry-server.tsx'
+    outDir: 'dist/client',
+    emptyOutDir: true,
+    manifest: true,
+    rollupOptions: {
+      input: {
+        main: './index.html'  // Explicit input path
+      }
+    }
+  },
+  ssr: {
+    target: 'node',
+    noExternal: true
   }
-});
+})
