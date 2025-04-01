1f35f8e84be47fcf6e42d484b294d516f1d01ccb
Adam Dreier <adamdreier@adams-mbp.mynetworksettings.com>
2025-03-31 21:37:46 -0700

    Crushing the SEO game

--- frontend/src/assets/static/TherapyOptions/TherapyOptions.ts
+++ frontend/src/assets/static/TherapyOptions/TherapyOptions.ts
@@ -2,7 +2,7 @@ export const options = [
   {
     title: 'Vitality Drip',
     description: `The Vitality Drip is an energizing and rejuvenating IV infusion that supports overall wellness by enhancing immune function and improving hydration. It is designed to boost energy, promote detoxification, and help with recovery, leaving you feeling refreshed and revitalized.`,
-    imageUrl: '/images/VitalityCocktail.png',
+    imageUrl: '/images/VitalityCocktail.webp',
     includes: [
       "Vitamin C",
       "Zinc",
@@ -22,7 +22,7 @@ export const options = [
   {
     title: 'Hangover Recovery',
     description: `The Hangover Cocktail is an IV infusion designed to quickly rehydrate the body, replenish lost nutrients, and alleviate symptoms like nausea, headache, and fatigue after alcohol consumption. It helps restore electrolyte balance and boost energy levels for faster recovery.`,
-    imageUrl: '/images/HangoverCocktail.png',
+    imageUrl: '/images/HangoverCocktail.webp',
     includes: [
       "Anti-Pain Medicine (Toradol)",
       "Vitamin C",
@@ -39,7 +39,7 @@ export const options = [
   {
     title: 'Migraine Relief',
     description: `The Migraine Cocktail is an IV infusion designed to relieve migraine symptoms by replenishing essential vitamins and minerals that help reduce pain and inflammation. It provides fast relief by addressing dehydration, nutrient deficiencies, and muscle tension often associated with migraines.`,
-    imageUrl: '/images/MigraineCocktail.png',
+    imageUrl: '/images/MigraineCocktail.webp',
     includes: [
       "Normal Saline",
       "Anti-Pain Medicine (Toradol)",
@@ -54,7 +54,7 @@ export const options = [
   {
     title: 'Myers Cocktail',
     description: `The Myers Cocktail is an IV infusion that combines essential nutrients to support energy, immune function, and overall wellness. It is commonly used to treat fatigue, dehydration, migraines, and stress, providing fast relief and hydration.`,
-    imageUrl: '/images/meyerscocktail.png',
+    imageUrl: '/images/meyerscocktail.webp',
     includes: [
       "Vitamin C",
       "Vitamin B Complex",
@@ -71,7 +71,7 @@ export const options = [
   {
     title: 'High-Dose Vitamin C',
     description: `High-dose Vitamin C infusions are used to boost the immune system, enhance detoxification, and promote healing. This therapy helps to fight infections, reduce inflammation, and improve overall cellular health by delivering a concentrated dose directly into the bloodstream for maximum absorption.`,
-    imageUrl: '/images/VitaminC.png',
+    imageUrl: '/images/VitaminC.webp',
     includes: [
       "Vitamin C",
       "Normal Saline"
@@ -83,7 +83,7 @@ export const options = [
   {
     title: 'NAD+',
     description: 'NAD+ (Nicotinamide Adenine Dinucleotide) is a coenzyme involved in cellular energy production and metabolism, playing a key role in maintaining overall health and vitality. NAD+ infusions are used to support energy levels, improve cognitive function, reduce inflammation, and promote cellular repair.',
-    imageUrl: '/images/Nad.png',
+    imageUrl: '/images/Nad.webp',
     includes: [
       "Starting dose 100 mg"
     ],
@@ -94,7 +94,7 @@ export const options = [
   {
     title: 'Hydration IV',
     description: `Hydration therapy with IV solutions like Normal Saline (NS) or Lactated Ringer's (LR) helps restore fluids and electrolytes in the body. These infusions are commonly used to treat dehydration, improve energy, and support overall hydration balance.`,
-    imageUrl: '/images/Hydration.png',
+    imageUrl: '/images/Hydration.webp',
     includes: [
       "Normal Saline (NS) or Lactated Ringer's",
     ],
